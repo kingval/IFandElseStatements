a8a69377a991c47affa50cac5eb9760fa6dbaebb
kingval <king.valand@gmail.com>
2022-12-17 14:06:44 +0000

    udpate1.1

--- IfStatements.java
+++ IfStatements.java
@@ -4,8 +4,8 @@ public class IfStatements {
 
     public static void main(String[] args) {
 
-        int i = 80;
-        if(i<18){
+        int i = 100;
+        if(i<30){
             System.out.println("I is Small");
         }
         System.out.println("false");
